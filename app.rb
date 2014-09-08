3989690424462f3a8e3b0d0402f0c1268bd9ae85
sunmeg <atpope5@gmail.com>
2014-09-08 19:27:24 -0400

    Added case insensitive search functionality

--- app.rb
+++ app.rb
@@ -1,15 +1,20 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require "sinatra/activerecord"
+require 'pry'
 
 require_relative 'models/contact'
 
 get '/' do
-  @contacts = Contact.all
+  if params['query'] != nil
+    @contacts = Contact.where("first_name ILIKE ? OR last_name ILIKE ?", params['query'],params['query'])
+  else
+    @contacts = Contact.all
+  end
   erb :index
 end
 
 get '/contacts/:id' do
-  @contact = Contact.find(params[:id])
+  @contact = Contact.where("id = ?", params[:id])
   erb :show
 end
